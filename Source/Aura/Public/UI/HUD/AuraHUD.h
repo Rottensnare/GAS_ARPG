9780a361914608ddd3781e0a95d3f07efdcb057b
Roope P <49348290+Rottensnare@users.noreply.github.com>
2023-04-08 17:45:19 +0300

    HUD
    Added health globe and mana globe. Created HUD

---
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+	
+
+
+protected:
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> OverlayWidgetClass;
+
+	virtual void BeginPlay() override;
+	
+};
