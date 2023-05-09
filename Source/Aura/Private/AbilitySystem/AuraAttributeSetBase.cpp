119d8122b038847cc35681759ecc45dfaa2e8618
Roope P <49348290+Rottensnare@users.noreply.github.com>
2023-05-09 19:43:17 +0300

    Fixed clamping bug
    For whatever reason I had GetHealth instead of GetMaxHealth, and I had GetMaxHealth instead of GetMaxMana. I was probably tired, no other reason for this bad of an error.

--- Source/Aura/Private/AbilitySystem/AuraAttributeSetBase.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSetBase.cpp
@@ -33,11 +33,11 @@ void UAuraAttributeSetBase::PreAttributeChange(const FGameplayAttribute& Attribu
 
 	if(Attribute == GetHealthAttribute())
 	{
-		NewValue = FMath::Clamp(NewValue, 0, GetHealth());
+		NewValue = FMath::Clamp(NewValue, 0, GetMaxHealth());
 	}
 	else if(Attribute == GetManaAttribute())
 	{
-		NewValue = FMath::Clamp(NewValue, 0, GetMaxHealth());
+		NewValue = FMath::Clamp(NewValue, 0, GetMaxMana());
 	}
 }
 
@@ -54,9 +54,9 @@ void UAuraAttributeSetBase::PostGameplayEffectExecute(const FGameplayEffectModCa
 void UAuraAttributeSetBase::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
 {
 	Props.EffectContextHandle = Data.EffectSpec.GetContext();
-	Props.SourceASC =  Data.EffectSpec.GetContext().GetOriginalInstigatorAbilitySystemComponent();
+	Props.SourceASC = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
 
-	if(Props.SourceASC && Props.SourceASC->AbilityActorInfo.IsValid() && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
+	if (IsValid(Props.SourceASC) && Props.SourceASC->AbilityActorInfo.IsValid() && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
 	{
 		Props.SourceAvatarActor = Props.SourceASC->AbilityActorInfo->AvatarActor.Get();
 		Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
