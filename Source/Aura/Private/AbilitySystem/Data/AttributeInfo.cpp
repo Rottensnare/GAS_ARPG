8c718bdcbe9547b5b618ce64ef7450332e551d52
Roope P <49348290+Rottensnare@users.noreply.github.com>
2023-05-21 17:55:29 +0300

    AttributeInfo
    Added AttributeInfo struct and class and created a data asset based on it.

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for(const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if(Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+
+	if(bLogNotFound) UE_LOG(LogTemp, Warning, TEXT("Can't find info for AttributeTag: %s"), *AttributeTag.ToString())
+
+	return FAuraAttributeInfo();
+}
