ab76e3f61561bcdfe8ecc26deec4994b9f9e542a
Roope P <49348290+Rottensnare@users.noreply.github.com>
2023-05-21 17:05:01 +0300

    Asset manager, gameplay tags
    Added a custom asset manager. Changed gameplay tags to be native gameplay tags. Attribute menu can now be opened using the attribute menu button.

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponentBase.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponentBase.cpp
@@ -3,8 +3,10 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponentBase.h"
 
+#include "AuraGameplayTags.h"
+
 void UAuraAbilitySystemComponentBase::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
-	const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
+                                                    const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
 {
 	FGameplayTagContainer GPTagContainer;
 	EffectSpec.GetAllAssetTags(GPTagContainer);
@@ -16,4 +18,7 @@ void UAuraAbilitySystemComponentBase::EffectApplied(UAbilitySystemComponent* Abi
 void UAuraAbilitySystemComponentBase::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &ThisClass::EffectApplied);
+
+	//const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+	//UE_LOG(LogTemp, Warning, TEXT("Tag: %s"), *GameplayTags.Attribute_Secondary_Armor.ToString())
 }
